c15a239903fe08d9d00accb699a3fcc0bf66d439
Ryan <ryandsantosoliveira@gmail.com>
2026-03-29 19:46:19 -0300

    melhorando interface

--- janela.py
+++ janela.py
@@ -3,5 +3,16 @@ import customtkinter as ctk
 ctk.set_appearance_mode('Dark')
 
 app = ctk.CTk()
-app.geometry('500x500')
+app.title("Conversor de bases")
+app.geometry('600x400')
+
+titulo = ctk.CTkLabel(app, text='Número')
+titulo.pack(pady='15px')
+
+numero = ctk.CTkEntry(app, placeholder_text='Digite o número a ser convertido',width=200 )
+numero.pack()
+
+
+
 app.mainloop()
+
