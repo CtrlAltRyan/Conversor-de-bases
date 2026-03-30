9bea5a01df092fa32ffc31bb3c528ed985a941ee
Ryan <ryandsantosoliveira@gmail.com>
2026-03-29 23:21:00 -0300

    resolvido problemas de input na calculadora e interface aprimorada

--- main.py
+++ main.py
@@ -6,26 +6,32 @@ app = ctk.CTk()
 app.title("Conversor de bases")
 app.geometry('600x400')
 
-titulo = ctk.CTkLabel(app, text='Conversor de bases')
-titulo.pack(pady='10px')
-
-numInicial = ctk.CTkEntry(app, placeholder_text='Digite o número a ser convertido', width=200)
-numInicial.pack(pady='6px')
-
 bases = ["Binário", "Octal", "Decimal", "Hexadecimal", "Outro"]
+
+titulo = ctk.CTkLabel(app, text='Digite o número a ser convertido e selecione sua base')
+titulo.pack(pady=(10,0))
+
+frame = ctk.CTkFrame(app) ## 1 campo
+frame.pack(pady=(0,0))
+
+numInicial = ctk.CTkEntry(frame, placeholder_text='Numero', width=200)
+numInicial.pack(side='left',pady=(5,5), padx='5px')
+
+frameInvisivel = ctk.CTkFrame(app, fg_color="transparent",height=28) ## 1 campo
+frameInvisivel.pack(pady=(0,15))
+
 
 def outroInicialSelecionado(valor):
     if valor == "Outro":
-        if not outroBaseInicial.winfo_ismapped():
-            outroBaseInicial.pack(after=baseInicial, pady=6)
-            outroBaseInicial.focus()
+        outroBaseInicial.pack(pady=(0))
+        outroBaseInicial.focus()
     else:
         outroBaseInicial.pack_forget()
 
-baseInicial = ctk.CTkSegmentedButton( app, values=bases, command=outroInicialSelecionado)
-baseInicial.pack(pady='6px')
+baseInicial = ctk.CTkSegmentedButton(frame, values=bases, command=outroInicialSelecionado)
+baseInicial.pack(side='right', pady='6px')
 
-outroBaseInicial = ctk.CTkEntry( app, placeholder_text="(max: base 36)...")
+outroBaseInicial = ctk.CTkEntry(frameInvisivel, placeholder_text=" ")
 
 def outroPretendidaSelecionado(valor):
     if valor == "Outro":
@@ -35,9 +41,11 @@ def outroPretendidaSelecionado(valor):
     else:
         outroBasePretendida.pack_forget()
 
+mensagem = ctk.CTkLabel(app, text='Selecione a base desejada')
+mensagem.pack()
 
 basePretendida = ctk.CTkSegmentedButton(app, values=bases, command=outroPretendidaSelecionado)
-basePretendida.pack(pady='6px')
+basePretendida.pack(pady=(0,5))
 
 outroBasePretendida = ctk.CTkEntry(app, placeholder_text="Digite a Base Pretendida...")
 
@@ -51,18 +59,32 @@ def genericoBase() -> str:
     num1 = str(numInicial.get())
     base10 = 0
 
-    if baseInicial.get() == "Outro": base1 = int(outroBaseInicial.get())
+    if baseInicial.get() == "Outro": 
+        base1 = int(outroBaseInicial.get())
+        if base1 > 36 or base1 < 1:
+            resultado.configure(text='Digite uma base válida')
+            return
     else:
         base1 = int(basesdict[baseInicial.get()])
 
-    if basePretendida.get() == "Outro": baseDesejada = int(outroBasePretendida.get())
+    if basePretendida.get() == "Outro": 
+        baseDesejada = int(outroBasePretendida.get())
+        if baseDesejada > 36 or baseDesejada < 1:
+            resultado.configure(text='Digite uma base válida')
+            return
     else:
         baseDesejada = int(basesdict[basePretendida.get()])
+
 
     for n, i in enumerate(num1[::-1]):  # para decimal
         if not i.isdigit():
             i = (list(dict.values()).index(i.upper()) + 10)
         base10 += base1 ** n * int(i)
+    
+    if base10 < baseDesejada:
+        resultado.configure(text=f'Número não representável nessa base')
+        return
+
 
     resposta = str()
 
